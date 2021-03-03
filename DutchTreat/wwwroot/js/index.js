31d890c3b439d8895f81341bd5b841609fbd2350
LukaszSmoczyk <lukaszsmoczyk@gmail.com>
2021-03-03 22:31:09 +0100

    js basics

---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,13 @@
+﻿
+console.log("Hello Pluralsight!");
+
+var theForm = document.getElementById("theForm");
+theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function() {
+    alert("Buying Item");
+});
+
+var productInfo = document.getElementsByClassName("product-props");
+// var listImtes = productInfo.item[0].children;
