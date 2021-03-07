6f3ba39abcb17c76ba9bc7c0be2128cd0d33b673
LukaszSmoczyk <lukaszsmoczyk@gmail.com>
2021-03-07 22:18:30 +0100

    mvc start

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,13 +1,23 @@
-﻿
-console.log("Hello Pluralsight!");
+﻿$(document).ready(function () {
+    console.log("Hello Pluralsight!");
 
-var theForm = document.getElementById("theForm");
-theForm.hidden = true;
+    var theForm = $("#theForm");
+    theForm.hide();
 
-var button = document.getElementById("buyButton");
-button.addEventListener("click", function() {
-    alert("Buying Item");
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.slideToggle(1000);
+    });
 });
-
-var productInfo = document.getElementsByClassName("product-props");
-// var listImtes = productInfo.item[0].children;
