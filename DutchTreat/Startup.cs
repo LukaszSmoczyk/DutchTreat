cbbc4dfbd66fde13679fa982e43ae1e897a08483
LukaszSmoczyk <lukaszsmoczyk@gmail.com>
2021-03-21 23:45:02 +0100

    db migration

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -1,7 +1,10 @@
+using DutchTreat.Data;
 using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -13,10 +16,19 @@ namespace DutchTreat
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DutchContext>(options =>
+                options.UseSqlServer(_config.GetConnectionString("DefaultConnection")));
+
             services.AddTransient<IMailService, NullMailService>();
             services.AddControllersWithViews();
             services.AddRazorPages();
